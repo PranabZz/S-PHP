3f517859f669ae564313cad5b05d028e5a571694
PranabZz <pranabkca321@gmail.com>
2025-03-01 17:18:10 +0545

    fix: cli fixed with command classes

--- function.php
+++ function.php
@@ -1,6 +1,7 @@
 <?php
 
 
+
 session_start();
 
 error_reporting(E_ALL & ~E_WARNING);
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'pranab/s-php',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'bc84a8464671550319e0caae5a892bdb7960f599',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'pranab/s-php' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'bc84a8464671550319e0caae5a892bdb7960f599',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
